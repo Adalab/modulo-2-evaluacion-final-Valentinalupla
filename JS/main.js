e68704a9ce1f2a0e1b6fec4fa954cccf5c109458
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-03-05 12:31:42 +0100

    JS in process

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,57 @@
 'use strict';
-inputSearch = document.querySelector('.js-search');
-buttonSearch = document.querySelector('.js-button');
-
+const inputSearch = document.querySelector('.js-search');
+const buttonSearch = document.querySelector('.js-button');
+const containerSerie = document.querySelector('.js-container');
 const url = 'https://api.jikan.moe/v4/anime?q=naruto';
-let animeSeries = [];
+let titleSeries = [];
 let favoriteSerie = [];
 
-/* Renderizar búsqueda */
-const renderSeries = () => {
+const renderSeries = (data) => {
     let content = '';
-    for(const serie of series) {
-        content += `<div class="js-serie"> 
-        <img class="js-img"/>
-        <h3>${serie.title}</h3>
-        </div>`;
+    
+    for(const serie of data){
+        containerSerie.innerHTML = `<div id="${serie.id}">
+            <h3>${serie.title}</h3>
+        `
+         for(const title of serie.titles){
+            // console.log(title);
+            content += `<div ${title}></div> `
+        }
+       content += '</div>'
     }
-    content += '</div>';
+   containerSerie.innerHTML = content;
 }
-
 
 
 
 /* Pintar las series con la info del servidor */
 
-const dataOfSeries = () => {
     fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-        animeSeries = data.series;
-        console.log(data);
-        localStorage.setItem('seriesAnime', JSON.stringify(animeSeries));
+    .then((allData) => {
+        console.log('ha hecho una peticion al servidor');
+       const titleSeries = allData.data; //array
+        renderSeries(titleSeries);
+        localStorage.setItem('seriesAnime',JSON.stringify(titleSeries));
     });
-};
 
-    // const dataSeriesLS = JSON.parse(localStorage.getItem('seriesAnime'));
+    const titleSeriesLS = JSON.parse(localStorage.getItem('seriesAnime'));
+    console.log(titleSeriesLS);
+
+ 
+
+const handleclick = (event) => {
+   event.preventDefault();
+    const clickButton = inputSearch.value;
+    console.log('click');
+}
+    const searchSeries = titleSeries.filter((serie) => {
+        return serie.title.includes(clickButton);
+    })
 
 
 
+buttonSearch.addEventListener('click', handleclick);
 
 
-
-    buttonSearch.addEventListener('click' handleSearch);
+  
