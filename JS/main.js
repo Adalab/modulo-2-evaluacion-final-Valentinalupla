cd03a7f1807ed13ea7dae3259f77d8f5e67d2620
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-03-05 13:17:31 +0100

    function search done

--- JS/main.js
+++ JS/main.js
@@ -2,52 +2,44 @@
 const inputSearch = document.querySelector('.js-search');
 const buttonSearch = document.querySelector('.js-button');
 const containerSerie = document.querySelector('.js-container');
-const url = 'https://api.jikan.moe/v4/anime?q=naruto';
+
 let titleSeries = [];
 let favoriteSerie = [];
 
+/* Pintar las series en el html cuando la busquen*/
 const renderSeries = (data) => {
     let content = '';
     
     for(const serie of data){
-        containerSerie.innerHTML = `<div id="${serie.id}">
-            <h3>${serie.title}</h3>
-        `
-         for(const title of serie.titles){
-            // console.log(title);
-            content += `<div ${title}></div> `
-        }
-       content += '</div>'
+        content += `<div>
+                <img src="${serie.images.jpg.image_url}"/>
+                  <h3>${serie.title}</h3>`;
+       content += '</div>';
     }
    containerSerie.innerHTML = content;
 }
 
 
 
-/* Pintar las series con la info del servidor */
+/* Buscar las series con la info del servidor */
 
+const handleclick = (event) => {
+    event.preventDefault();
+    const clickButton = inputSearch.value;
+    const url = `https://api.jikan.moe/v4/anime?q=naruto ${clickButton}`;
+    
     fetch(url)
     .then((response) => response.json())
     .then((allData) => {
-        console.log('ha hecho una peticion al servidor');
        const titleSeries = allData.data; //array
         renderSeries(titleSeries);
         localStorage.setItem('seriesAnime',JSON.stringify(titleSeries));
     });
 
     const titleSeriesLS = JSON.parse(localStorage.getItem('seriesAnime'));
-    console.log(titleSeriesLS);
-
- 
-
-const handleclick = (event) => {
-   event.preventDefault();
-    const clickButton = inputSearch.value;
-    console.log('click');
+  
 }
-    const searchSeries = titleSeries.filter((serie) => {
-        return serie.title.includes(clickButton);
-    })
+
 
 
 
