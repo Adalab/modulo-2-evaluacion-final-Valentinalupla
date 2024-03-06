69929ccad3f3834cd88148f27fa6824fc856734a
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-03-06 11:30:01 +0100

    all functionality it's ok

--- JS/main.js
+++ JS/main.js
@@ -8,37 +8,36 @@ let titleSeries = [];
 let favoriteSerie = [];
 
 /* Guardar serie favorita en LocalStorage */
-// const favoriteSerieLS = () => {
-//     const favStorage = localStorage.getItem('favoriteSerie');
-//     if(favStorage){
-//         favoriteSerie = JSON.parse(favStorage);
-        
-//     }
-// }
 const saveFavSerieLS = () => {
     localStorage.setItem('favoriteSerie', JSON.stringify(favoriteSerie));
-    console.log(saveFavSerieLS);
 }
+const favoriteSerieLS = () => {
+    const favStorage = localStorage.getItem('favoriteSerie');
+    if(favStorage){
+        favoriteSerie = JSON.parse(favStorage);
+        console.log(favoriteSerie);
+    }
+}
+favoriteSerieLS();
+
 
 /*Click serie favorita |FAVORITOS|  */
 const handleAddFavorite = (event) => {
     event.preventDefault();
    const selectedSerie = titleSeries.find((serie) => {
     return parseInt(event.currentTarget.id) === serie.mal_id;
-    });
+    }); console.log('selectedSerie', selectedSerie);
     // buscar si el elemento seleccionado ya existe en favoritos
-    const indexFavSerie = favoriteSerie.findIndex((favoriteTitle) => {
-        return favoriteTitle.mal_id === selectedSerie.mal_id;
-    });
-    console.log(favoriteSerie);
-    if(indexFavSerie === -1){
-        saveFavSerieLS();
-        favoriteSerie.push(selectedSerie);
-        event.currentTarget.style.backgroundColor = 'greenyellow';
-        event.currentTarget.style.color = 'deeppink';
-    };
+     const indexFavSerie = favoriteSerie.findIndex((favoriteTitle) => {
+         return favoriteTitle.mal_id === selectedSerie.mal_id;
+     });
 
-renderSeries(favoriteSerie, containerFavoriteSerie);   /*Renderiza las series */
+    if(indexFavSerie === -1){
+    favoriteSerie.push(selectedSerie);
+    event.currentTarget.style.backgroundColor = 'greenyellow';
+    event.currentTarget.style.color = 'deeppink';
+    saveFavSerieLS();}
+    renderSeries(favoriteSerie, containerFavoriteSerie);   /*Renderiza las series */
 }
 
 
@@ -48,12 +47,12 @@ const fillerImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
 
 const renderSeries = (data, containerFavoriteSerie) => {
     let content = '';
-    
+    const urlTwo = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
     for(const serie of data){
-     const imageUrl = serie.images.jpg.image_url ? serie.images.jpg.image_url : fillerImage;
+     const imageUrl = serie.images.jpg.image_url !== urlTwo ? serie.images.jpg.image_url : fillerImage;
 
         content += ` <div class="individual-serie js-serie" id="${serie.mal_id}">
-                <img src="${serie.images.jpg.image_url}"/>
+                <img src="${imageUrl}"/>
                   <h3>${serie.title}</h3>`;
         content += '</div>';
     }
@@ -63,6 +62,7 @@ const renderSeries = (data, containerFavoriteSerie) => {
     containerSerie.addEventListener('click', handleAddFavorite);
    }
 }
+renderSeries(favoriteSerie, containerFavoriteSerie); 
 
 
 
@@ -71,7 +71,7 @@ const renderSeries = (data, containerFavoriteSerie) => {
 const handleclick = (event) => {
     event.preventDefault();
     const clickButton = inputSearch.value;
-    const url = `https://api.jikan.moe/v4/anime?q=naruto ${clickButton}`;
+    const url = `https://api.jikan.moe/v4/anime?q=${clickButton}`;
     
     fetch(url)
     .then((response) => response.json())
@@ -81,10 +81,8 @@ const handleclick = (event) => {
         localStorage.setItem('seriesAnime',JSON.stringify(titleSeries));
     });
 
-    const titleSeriesLS = JSON.parse(localStorage.getItem('seriesAnime'));
   
 }
-
 
 buttonSearch.addEventListener('click', handleclick);
 
